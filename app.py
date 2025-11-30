bb073a641100b1733b35addb002e966705307697
Arun-R-S <arunrs0966@gmail.com>
2025-11-30 21:52:06 +0530

    Added a timedelay bewteen each cycle

--- app.py
+++ app.py
@@ -383,6 +383,7 @@ try:
                                 cfg["actionName"],
                                 telemetry
                             )
+			time.sleep(0.2)
                     elif isGamePaused:
                         printDebugWarn("Game Paused")
             else:
